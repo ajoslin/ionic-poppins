cbf69ad1d3539db63ddaa8b135e1dd453e53e00d
Andy Joslin <andytjoslin@gmail.com>
2014-03-31 21:39:27 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = function (poppins) {
+  poppins.config = {
+    // Github repo to watch
+    // https://github.com/myname/myrepo
+    target: {
+      user: 'ajoslin',
+      repo: 'angular-promise-tracker'
+    },
+
+    // Credentials for user who leaves comments, etc.
+    // You may want to load these from a seperate file like `config-credentials.js`, and
+    // add this file to your `.gitignore` list
+    login: require('./crendentials.json'),
+
+    // port for poppins to listen on and URL for Github to ping
+    hook: {
+      url: 'http://54.221.213.146',
+      port: 4321
+    }
+  };
+
+  poppins.on('issueOpened', function(data) {
+    console.log(data);
+  });
+};
