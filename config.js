6cac53a970d25890ba01ab4e122cb378bd91b6df
Andy Joslin <andytjoslin@gmail.com>
2014-03-31 21:41:28 +0000

    typo

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = function (poppins) {
     // Credentials for user who leaves comments, etc.
     // You may want to load these from a seperate file like `config-credentials.js`, and
     // add this file to your `.gitignore` list
-    login: require('./crendentials.json'),
+    login: require('./credentials.json'),
 
     // port for poppins to listen on and URL for Github to ping
     hook: {
